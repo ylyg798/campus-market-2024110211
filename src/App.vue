12ad3320c6c11778f4cf99221094234d8fec69c6
1592720048 <1592720048@qq.com>
2026-06-28 15:19:02 +0800

    Day2 Task5：修改 App.vue
    - 简化App.vue使用统一布局组件
    
    - 所有页面显示在统一的顶部导航和内容容器中

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,7 @@
-<script setup lang="ts">
-import { onMounted, computed } from 'vue'
-import { useUserStore } from '@/stores/userStore'
-import AppLayout from '@/components/AppLayout.vue'
-import SetupView from '@/views/SetupView.vue'
-
-const userStore = useUserStore()
-
-const isLoggedIn = computed(() => userStore.user)
-
-onMounted(() => {
-  userStore.loadUser()
-})
-</script>
-
 <template>
-  <div id="app">
-    <template v-if="isLoggedIn">
-      <AppLayout />
-    </template>
-    <template v-else>
-      <SetupView />
-    </template>
-  </div>
+  <AppLayout />
 </template>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: 'PingFang SC', 'Microsoft YaHei', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
-  background: #f5f7fb;
-  color: #1e293b;
-  line-height: 1.6;
-}
-
-#app {
-  min-height: 100vh;
-}
-</style>
+<script setup lang="ts">
+import AppLayout from './components/AppLayout.vue'
+</script>
